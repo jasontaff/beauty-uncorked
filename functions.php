fb6f434284400debf0ce31ee667846b5c2d791df
jasontaff <jason.dion.taff@gmail.com>
2019-06-23 15:55:01 -0400

    update files

--- functions.php
+++ functions.php
@@ -1,18 +1,14 @@
 <?php
 
 
-add_action('wp_enqueue_scripts','beauty_uncorked_scripts');
+function add_theme_scripts() {
+   
+   
+    //wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all');
 
-
-function beauty_uncorked_scripts() {
-
-     // StyleS
-     wp_enqueue_style( 'css',   get_template_directory_uri() . '/scss/main.css');
-
-     // Scripts
-     wp_enqueue_script( 'bootstrap.js', get_template_directory_uri() . '/js/bootstrap.min.js');
-     wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/main.js');
-
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array ( 'jquery' ), 1.0, true);
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array ( 'jquery' ), 1.0, true);
+   
+     
   }
-  
-  
+  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
