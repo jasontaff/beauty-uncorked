c7dc63acfc07c4ecd338e8dafe08d2051984c19c
jasontaff <jason.dion.taff@gmail.com>
2019-07-22 21:25:54 -0400

    classes added

--- header.php
+++ header.php
@@ -7,7 +7,10 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
     <link href="<?php echo get_stylesheet_directory_uri()?>/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    
     <?php wp_head(); ?>
 
 </head>
