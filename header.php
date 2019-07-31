1c3b75ca0713cd450bf3f62c789037ba136767af
jasontaff <jason.dion.taff@gmail.com>
2019-07-30 21:02:31 -0400

    update
    update

--- header.php
+++ header.php
@@ -24,6 +24,6 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
     the_content();
     endwhile; else: ?>
-    <p>Sorry, no posts matched your criteria.</p>
+    
 <?php endif; ?>
  
