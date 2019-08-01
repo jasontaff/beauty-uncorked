103a681fa867473bc662faad4a1518dc5dcef1c4
jasontaff <jason.dion.taff@gmail.com>
2019-07-31 23:27:57 -0400

    verison bump

--- functions.php
+++ functions.php
@@ -5,12 +5,12 @@ function add_theme_scripts() {
  
   
     //styles 
-     wp_enqueue_style( 'main-style', get_template_directory_uri() . '/media/scss/main.css', false, 1.1, 'all' );
+     wp_enqueue_style( 'main-style', get_template_directory_uri() . '/media/scss/main.css', false, 1.2, 'all' );
     
   
     //scripts
-    wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . '/media/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/media/js/main.js', array ( 'jquery' ), 1.0, true);
+    wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . '/media/js/bootstrap.min.js', array ( 'jquery' ), 1.2, true);
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/media/js/main.js', array ( 'jquery' ), 1.1, true);
    
      
   }
